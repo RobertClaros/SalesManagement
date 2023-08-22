bae415cf52aed4ecd7ceac5f5eb49dd4a6684d48
robertdev <robert.claros2009@gmail.com>
2023-08-22 17:28:06 -0400

    Se agrego el proyecto en Springboot

---
+++ salesManagement/src/main/java/com/ATL/salesManagement/SalesManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ATL.salesManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesManagementApplication.class, args);
+	}
+
+}
